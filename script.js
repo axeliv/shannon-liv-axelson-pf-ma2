73e5d8f7acdb7b6ef6f72552e59239b24c696584
axeliv <axeliv@users.noreply.github.com>
2020-05-31 16:35:37 +0200

    Add files via upload
    Commit FEU1/PF/MA2

---
+++ script.js
@@ -0,0 +1,37 @@
+//question 1
+var petArray = ["cat", "cow", "dog"];
+//question 2
+console.log(petArray[1]);
+//question 3
+petArray.push("sheep");
+//question 4
+console.log(petArray.length);
+//question 5
+var catObject = {
+  name: "Pinka",
+  color: "mottled",
+  age: 4,
+};
+//question 6
+console.log(catObject.age);
+//question 7
+var catArray = [
+  {
+    name: "Pinka",
+    color: "mottled",
+    age: 4,
+  },
+];
+//question 8
+for (var i = 0; i <= catArray.length; i++);
+{
+  console.log(catObject);
+}
+//question 9
+function logToConsole(animal) {
+  var personalAroma =
+    "I just finished a workout, and now I smell like a " + animal;
+  console.log(personalAroma);
+}
+//question 10
+logToConsole("donkey");
